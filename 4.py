88a285f5751e75d6dd1b5b2bbe74e24ec15e23b0
Pradeep <deeppradeep1998@gmail.com>
2024-08-05 20:31:02 +0530

    first commit

---
+++ 4.py
@@ -0,0 +1,17 @@
+# Question.1 you are given below string take the sum of all the sale amount
+# please nate that string object may have space as well!
+
+
+sales = '100, unknown, unknown,     20, 20, 10'
+
+my_list=sales.split(',')
+#print(my_list)
+
+total=0
+for item in my_list:
+    item1 = item.strip()
+    try:
+        total += int(item1)
+    except ValueError:
+        pass
+print(total)
